55f1b189955f6ce46b574d46d085a9ebe9969bb2
Naman13112004 <njudaipur04@gmail.com>
2026-03-28 12:32:55 +0530

    user post endpoint added

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ app.get("/", (req, res) => {
     res.send("Health Check...");
 });
 
+app.post("/user", (req, res) => {
+    res.json({
+        "name": "naman",
+        "id": 1
+    });
+});
+
 app.listen(3000, () => {
     console.log("Server running on port 3000.");
 });
